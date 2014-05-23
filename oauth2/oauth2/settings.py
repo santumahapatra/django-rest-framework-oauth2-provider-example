4aed53fe28a6029ded9ffc830829004a233fd08b
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-23 13:20:05 +0530

    Adding views for users, removing south package right now.

--- oauth2/oauth2/settings.py
+++ oauth2/oauth2/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'provider',
     'provider.oauth2',
-    'south',
+    #'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
