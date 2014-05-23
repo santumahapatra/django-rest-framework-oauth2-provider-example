ecb139ab11a1a18bade16ba14fa701972a546192
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-23 13:06:50 +0530

    Adding users app

--- oauth2/oauth2/settings.py
+++ oauth2/oauth2/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'users',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
