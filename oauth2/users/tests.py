a8af9c7ad71cfa7ffc282e1459f11cece0fb3d03
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-23 15:55:53 +0530

    Adding tests for user authentication

--- oauth2/users/tests.py
+++ oauth2/users/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .serializers import UserSerializer
+
+
+class CreateUserTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
+        self.client.login(username='john', password='johnpassword')
+        self.data = {'username': 'mike', 'first_name': 'Mike', 'last_name': 'Tyson'}
+
+    def test_can_create_user(self):
+        response = self.client.post(reverse('user-list'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class ReadUserTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
+        self.client.login(username='john', password='johnpassword')
+        self.user = User.objects.create(username="mike")
+
+    def test_can_read_user_list(self):
+        response = self.client.get(reverse('user-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_read_user_detail(self):
+        response = self.client.get(reverse('user-detail', args=[self.user.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class UpdateUserTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
+        self.client.login(username='john', password='johnpassword')
+        self.user = User.objects.create(username="mike", first_name="Tyson")
+        self.data = UserSerializer(self.user).data
+        self.data.update({'first_name': 'Changed'})
+
+    def test_can_update_user(self):
+        response = self.client.put(reverse('user-detail', args=[self.user.id]), self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class DeleteUserTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
+        self.client.login(username='john', password='johnpassword')
+        self.user = User.objects.create(username="mikey")
+
+    def test_can_delete_user(self):
+        response = self.client.delete(reverse('user-detail', args=[self.user.id]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
